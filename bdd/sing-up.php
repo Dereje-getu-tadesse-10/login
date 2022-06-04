de4f49fc7ea698f29d1ed0baa1448687a97da395
Dereje <getutadessedereje@icloud.com>
2022-06-04 21:54:52 +0200

    Fix: login si user existe ou pas

---
+++ bdd/sing-up.php
@@ -0,0 +1,37 @@
+<?php
+
+$errs = [];
+$regEx = "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/";
+
+// mail vide + regex
+if (empty($_POST['email'])) {
+    $errs[] = "Email is required";
+} else if (!preg_match($regEx, $_POST['email'])) {
+    $errs[] = "Email is not valid";
+} else if (empty($_POST['password'])) {
+    $errs[] = "Password is required";
+} else if (strlen($_POST['password']) < 8) {
+    $errs[] = "Password is too short";
+} else {
+    $emailSingUp = strip_tags($_POST['email']);
+    $passwordSingUp = strip_tags($_POST['password']);
+}
+
+if (empty($errs)) {
+    require_once 'conn.php';
+    $hashedPass = password_hash($passwordSingUp, PASSWORD_DEFAULT);
+    $sql = 'INSERT INTO `user` (`email`, `mot_de_passe`) VALUES (:email, :password)';
+    $query = $conn->prepare($sql);
+    $query->bindValue(':email', $emailSingUp, PDO::PARAM_STR);
+    $query->bindValue(':password', $hashedPass, PDO::PARAM_STR);
+    $query->execute();
+
+    if ($query) {
+        $success = array('success' => "inscription reussi");
+        echo json_encode($success);
+    } else {
+        $failed = array('failed' => "inscription echoue");
+        echo json_encode($failed);
+    }
+
+}
---
+++ login.js
@@ -0,0 +1,32 @@
+const formLogin = document.querySelector('#login')
+const email = document.querySelector('.email-log')
+const password = document.querySelector('.password-log')
+const errMail = document.querySelector('.errMail')
+const errPass = document.querySelector('.errPass')
+const existOrnot = document.querySelector('.exist-not-exist')
+console.log(formLogin);
+formLogin.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let emailValue = email.value
+    let passwordValue = password.value
+
+    const data = new FormData()
+
+    data.append('email-log', emailValue)
+    data.append('password-log', passwordValue)
+    let config = {
+        method: "POST",
+        body: data
+    }
+    fetch('login.php', config)
+    .then((res)=>{return res.json()})
+    .then((data)=>{
+        if(data.success){
+            window.location.href = "welcome.php"
+            console.log("connecter");
+        }else{
+            existOrnot.textContent = 'utilisateur n\'existe pas !';
+        }
+        console.log(data);
+    })
+})
